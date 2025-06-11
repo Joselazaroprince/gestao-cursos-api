d1b2cc1c8d2ecb9a5de99e79aab4aec8e1207eb0
José Lázaro Prince <joselazarocassoma3@gmail.com>
2025-06-11 15:00:10 +0100

    gestão de cursos

--- gestao_cursos/settings.py
+++ gestao_cursos/settings.py
@@ -1,16 +1,19 @@
 import os
 from pathlib import Path
 
+# BASE DIR
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-secret-key'
-DEBUG = True
-ALLOWED_HOSTS = ['joselazaroprince.pythonanywhere.com']
+# SEGURANÇA
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'chave-insegura-desenvolvimento')
+DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
+ALLOWED_HOSTS = [
+    'joselazaroprince.pythonanywhere.com',
+    'www.joselazaroprince.pythonanywhere.com'
+]
 
-# SOLUÇÃO: Adicionei esta configuração para resolver os warnings
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+# APLICATIVOS
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -22,6 +25,7 @@ INSTALLED_APPS = [
     'core',
 ]
 
+# MIDDLEWARE
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -32,46 +36,92 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# CONFIGURAÇÕES DE TEMPLATES
 ROOT_URLCONF = 'gestao_cursos.urls'
 
 TEMPLATES = [{
     'BACKEND': 'django.template.backends.django.DjangoTemplates',
     'DIRS': [],
     'APP_DIRS': True,
-    'OPTIONS': {'context_processors': [
-        'django.template.context_processors.debug',
-        'django.template.context_processors.request',
-        'django.contrib.auth.context_processors.auth',
-        'django.contrib.messages.context_processors.messages',
-    ]},
+    'OPTIONS': {
+        'context_processors': [
+            'django.template.context_processors.debug',
+            'django.template.context_processors.request',
+            'django.contrib.auth.context_processors.auth',
+            'django.contrib.messages.context_processors.messages',
+        ],
+    },
 }]
 
 WSGI_APPLICATION = 'gestao_cursos.wsgi.application'
 
+# BANCO DE DADOS - PRODUÇÃO (PythonAnywhere)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'gestao_cursos',
-        'USER': 'root',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
+        'NAME': 'joselazaroprince$gestao_cursos',
+        'USER': 'joselazaroprince',
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'sua_senha_secreta'),
+        'HOST': 'joselazaroprince.mysql.pythonanywhere-services.com',
         'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
 
-AUTH_PASSWORD_VALIDATORS = []
+# CONFIGURAÇÕES INTERNACIONAIS
 LANGUAGE_CODE = 'pt-br'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
-STATICFILES_DIRS = [] 
 
+# ARQUIVOS ESTÁTICOS
+STATIC_URL = '/static/'
+STATIC_ROOT = '/home/joselazaroprince/gestao-cursos-api/static'
+STATICFILES_DIRS = []  # Apenas se usar diretórios adicionais
+
+# ARQUIVOS DE MÍDIA (caso use uploads)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# REST FRAMEWORK
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
+}
+
+# CAMPO PADRÃO DE AUTO INCREMENTO
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# SEGURANÇA ADICIONAL PARA PRODUÇÃO
+if not DEBUG:
+    SECURE_HSTS_SECONDS = 31536000
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+
+# LOG DE ERROS (opcional)
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'error.log'),
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
 }
