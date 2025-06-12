a78d96925f79014ca271bdbe604f3c5e8eeafeb3
José Lázaro Prince <joselazarocassoma3@gmail.com>
2025-06-12 15:03:11 +0100

    Gestao de cursos

--- gestao_cursos/__init__.py
+++
@@ -1,5 +0,0 @@
-# gestao_cursos_api/__init__.py
-import pymysql
-
-pymysql.install_as_MySQLdb()
-pymysql.version_info = (1, 4, 6, 'final', 0)  # Evita warnings
--- gestao_cursos/settings.py
+++ gestao_cursos/settings.py
@@ -1,46 +1,15 @@
-import os
-import environ
 from pathlib import Path
-import pymysql
-pymysql.install_as_MySQLdb()
 
-
-
-# BASE DIR
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5500",  # ou onde estiver o frontend
-    "https://joselazaroprince.pythonanywhere.com",
-]
+SECRET_KEY = 'django-insecure-secret-key'
+DEBUG = True
+ALLOWED_HOSTS = ['*']
 
+# SOLUÇÃO: Adicionei esta configuração para resolver os warnings
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Inicializa o django-environ
-env = environ.Env(DEBUG=(bool, False))
-
-# Carrega automaticamente .env.dev no desenvolvimento ou .env na produção
-if os.path.exists(os.path.join(BASE_DIR, '.env.dev')):
-    env.read_env(os.path.join(BASE_DIR, '.env.dev'))
-else:
-    env.read_env(os.path.join(BASE_DIR, '.env'))
-
-# SEGURANÇA
-SECRET_KEY = env('DJANGO_SECRET_KEY', default='chave-insegura-desenvolvimento')
-DEBUG = env('DEBUG', default=False)
-ENVIRONMENT = env('ENVIRONMENT', default='production')
-
-# ALLOWED HOSTS
-if ENVIRONMENT == 'production':
-    ALLOWED_HOSTS = [
-        'joselazaroprince.pythonanywhere.com',
-        'www.joselazaroprince.pythonanywhere.com'
-    ]
-else:
-    ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
-
-# APLICATIVOS
 INSTALLED_APPS = [
-    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -51,9 +20,7 @@ INSTALLED_APPS = [
     'core',
 ]
 
-# MIDDLEWARE
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -63,94 +30,45 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# CONFIGURAÇÕES DE TEMPLATES
 ROOT_URLCONF = 'gestao_cursos.urls'
 
 TEMPLATES = [{
     'BACKEND': 'django.template.backends.django.DjangoTemplates',
     'DIRS': [],
     'APP_DIRS': True,
-    'OPTIONS': {
-        'context_processors': [
-            'django.template.context_processors.debug',
-            'django.template.context_processors.request',
-            'django.contrib.auth.context_processors.auth',
-            'django.contrib.messages.context_processors.messages',
-        ],
-    },
+    'OPTIONS': {'context_processors': [
+        'django.template.context_processors.debug',
+        'django.template.context_processors.request',
+        'django.contrib.auth.context_processors.auth',
+        'django.contrib.messages.context_processors.messages',
+    ]},
 }]
 
 WSGI_APPLICATION = 'gestao_cursos.wsgi.application'
 
-# BANCO DE DADOS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'joselazaroprince$gestao_cursos',
-        'USER': 'joselazaroprince',
-        'PASSWORD': 'ENGENHEIRO',
-        'HOST': 'joselazaroprince.mysql.pythonanywhere-services.com',
+        'NAME': 'gestao_cursos',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
         'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
-        },
     }
 }
 
-
-# CONFIGURAÇÕES INTERNACIONAIS
+AUTH_PASSWORD_VALIDATORS = []
 LANGUAGE_CODE = 'pt-br'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
+STATIC_URL = 'static/'
 
-# ARQUIVOS ESTÁTICOS
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = []  # Apenas se usar diretórios adicionais
 
-# ARQUIVOS DE MÍDIA
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-# REST FRAMEWORK
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
-}
-
-# CAMPO PADRÃO DE AUTO INCREMENTO
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# SEGURANÇA ADICIONAL PARA PRODUÇÃO
-if not DEBUG:
-    SECURE_HSTS_SECONDS = 31536000
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_PRELOAD = True
-    SECURE_SSL_REDIRECT = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
-
-# LOG DE ERROS
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'ERROR',
-            'class': 'logging.FileHandler',
-            'filename': os.path.join(BASE_DIR, 'error.log'),
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    },
 }
