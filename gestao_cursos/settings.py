27893a4236c372b9cd7e1be7f9d3d9c519416a49
José Lázaro Prince <joselazarocassoma3@gmail.com>
2025-06-12 08:39:10 +0100

    Update settings.py

--- gestao_cursos/settings.py
+++ gestao_cursos/settings.py
@@ -74,16 +74,14 @@ WSGI_APPLICATION = 'gestao_cursos.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': env('DB_PORT'),
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        }
+        'NAME': 'joselazaroprince$gestao_cursos',
+        'USER': 'joselazaroprince',
+        'PASSWORD': '1234',
+        'HOST': 'joselazaroprince.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
     }
 }
+
 
 # CONFIGURAÇÕES INTERNACIONAIS
 LANGUAGE_CODE = 'pt-br'
