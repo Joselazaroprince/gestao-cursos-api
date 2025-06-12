4df95972a7945331ee3a96bbe9db1acddcc6d54e
José Lázaro Prince <joselazarocassoma3@gmail.com>
2025-06-12 09:09:13 +0100

    gestao de cursos

--- core/views.py
+++ core/views.py
@@ -1,22 +1,28 @@
-# core/views.py
-
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .models import Curso, Modulo, Inscricao
 from .serializers import CursoSerializer, ModuloSerializer, InscricaoSerializer
 
 class CursoViewSet(viewsets.ModelViewSet):
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['nome', 'descricao']
+    ordering_fields = ['nome']
 
 class ModuloViewSet(viewsets.ModelViewSet):
     queryset = Modulo.objects.all()
     serializer_class = ModuloSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['nome', 'curso__nome']
+    ordering_fields = ['nome']
 
 class InscricaoViewSet(viewsets.ModelViewSet):
     queryset = Inscricao.objects.all()
     serializer_class = InscricaoSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['estudante', 'curso__nome']
+    ordering_fields = ['estudante']
 
-# NOVA VIEW PARA FRONTEND
 def index(request):
     return render(request, 'index.html')
--- gestao_cursos/urls.py
+++ gestao_cursos/urls.py
@@ -1,17 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from core.views import index
-from rest_framework import routers
-from core.views import CursoViewSet, ModuloViewSet, InscricaoViewSet
-
-router = routers.DefaultRouter()
-router.register(r'cursos', CursoViewSet)
-router.register(r'modulos', ModuloViewSet)
-router.register(r'inscricoes', InscricaoViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    path('api/', include('core.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('', index),  # SPA entry point
+    path('', index),
 ]
